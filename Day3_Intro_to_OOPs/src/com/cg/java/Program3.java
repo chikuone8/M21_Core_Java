c9a359114afac95722bfb4a574d2e35f02aa2515
vipul <vipul@Vipul>
2022-07-15 09:04:53 +0530

    First Commit

---
+++ Day3_Intro_to_OOPs/src/com/cg/java/Program3.java
@@ -0,0 +1,16 @@
+package com.cg.java;
+class A
+{
+	A()
+	{
+		System.out.println("Hey There, I am using Constructor");
+	}
+}
+
+public class Program3 {
+
+	public static void main(String[] args) {
+		A obj = new A();
+	}
+
+}
---
+++ Day3_Intro_to_OOPs/src/com/cg/java/program2.java
@@ -0,0 +1,19 @@
+package com.cg.java;
+
+
+	class B
+	{
+		//public int x;
+		B(int a)
+		{
+			System.out.println("The value of a is :" +a);
+		}
+	}
+	public class program2 {
+	public static void main(String[] args) {
+		B obj = new B(15);
+	}
+
+}
+
+
