f5523908226639f0243aebe36d3e3d6d8683e3b0
vipul <vipul@Vipul>
2022-07-15 08:30:45 +0530

    First Commit

---
+++ Assignment_01/src/com/cg/java/Program2.java
@@ -0,0 +1,15 @@
+package com.cg.java;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+			for (int x = 2; x <= 4; x++)
+				 System.out.println("Value of x:" + x);
+
+		}
+
+	
+
+}
+
---
+++ Assignment_01/src/com/cg/java/Program5.java
@@ -0,0 +1,15 @@
+package com.cg.java;
+
+public class Program5 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++){ 
+			 if(i==5){ 
+			 break; 
+			 } 
+			 System.out.println(i); 
+			 }
+
+	}
+
+}
