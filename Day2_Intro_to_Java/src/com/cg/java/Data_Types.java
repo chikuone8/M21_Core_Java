6080b1de99e49191f0aa8ace5caf90e5d8193edb
vipul <vipul@Vipul>
2022-07-19 09:23:10 +0530

    Second Commit

---
+++ Day2_Intro_to_Java/src/com/cg/java/Data_Types.java
@@ -0,0 +1,20 @@
+package com.cg.java;
+
+public class Data_Types {
+
+	public static void main(String[] args) {
+		byte age = 34;
+        int age2 = 56;
+        short age3 = 87;
+        long ageDino = 5666666666666L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66;
+
+        boolean a = true;
+        System.out.println(age);
+        String str = "Vipul";
+        System.out.println(str);
+	}
+
+}
