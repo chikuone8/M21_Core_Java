be47de094cff4203debb2fd7a2a12ffe427d2199
vipul <vipul@Vipul>
2022-07-31 13:20:05 +0530

    First Commit

---
+++ Day16_JUnit/src/com/cg/testing/FirstUnitTestingProgram.java
@@ -0,0 +1,21 @@
+package com.cg.testing;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstUnitTestingProgram 
+{
+	
+	@Test
+	void accept()
+	{
+		int a=9;
+		System.out.println("Welcome to C2TC Program On Java Full Stack"+a);
+	}
+	@Test
+	void display()
+	{
+		int x=9;
+		System.out.println(x);
+	}
+
+}
